5e21aaf5420e19f748d13d191e19294cb512c562
t2botond <t2botond@gmail.com>
2016-12-13 19:43:53 +0100

    All exeercises ready except candy

--- week-08/day-2/callbacks/workshop/10.js
+++ week-08/day-2/callbacks/workshop/10.js
@@ -1,8 +1,14 @@
 // Create a circle with 100px diameter (use border radius)
 //  - Make it move from left to right edge
 //  - use requestAnimationFrame
-var c=document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-ctx.beginPath();
-ctx.arc(100,75,50,0,2*Math.PI);
-ctx.stroke();
+var requestAnimationFrame = window.requestAnimationFrame ||
+                            window.mozRequestAnimationFrame ||
+                            window.webkitRequestAnimationFrame ||
+                            window.msRequestAnimationFrame;
+
+function animateCircle(){
+  circle.x++;
+
+  window.requestAnimationFrame(animateCircle);
+}
+ animateCircle()
