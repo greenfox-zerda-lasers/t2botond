a2aaeb9239df44f1d418881b866197a2f8b000d4
t2botond <t2botond@gmail.com>
2016-12-26 09:51:34 +0100

    Holiday goes on

--- winter holiday/5.js
+++ winter holiday/5.js
@@ -37,3 +37,74 @@ function counter(list){
 // example: "apple" -> {a: 1, p: 2, l: 1, e: 1}
 
 var st = 'csak megirtam ezt a fost';
+
+function charsel(string){
+ var doe = {};
+ string.split('').forEach(function(element){
+   if (Object.keys(doe).indexOf(element) >= 0) {
+     doe[element] += 1;
+   }else{
+     doe[element] = 1;
+   }
+ })
+ return doe;
+ }
+ // console.log(charsel(st));
+
+var car = {
+  km: 120000,
+  ride: function(km){
+    this.km += km;
+  }
+}
+car.ride(200);
+// console.log(car.km);
+// create a student object
+// that has a method `addgrade`, that takes a grade from 1 to 5
+// an other method `getAverage`, that returns the average of the grades
+var student = {
+  grades: [],
+  addGrade: function(grade){
+    this.grades[this.grades.length] = grade
+  },
+  getAv: function(){
+    return  this.grades.reduce(function(accu, item){return accu + item}) / this.grades.length
+  }
+}
+student.addGrade(5);
+student.addGrade(2);
+student.addGrade(4);
+student.addGrade(2);
+// console.log(student.getAv());
+
+// Create a `Stack` constructor that stores elements
+// It should have a `size` method that returns number of elements it has
+// It should have a `push` method that adds an element to the stack
+// It should have a `pop` method that returns the last element form the stack and also deletes it from it
+
+// please don`t use the built in methods
+
+var Stack = {
+  elementlist: [],
+  sizeof: function(){
+    return this.elementlist.length
+  },
+  push: function(newitem){
+    this.elementlist[this.elementlist.length] = newitem
+  },
+  pop: function(){
+    var delitem;
+    delitem = this.elementlist[this.elementlist.length];
+    this.elementlist.length = this.elementlist.length - 1;
+  }
+}
+
+Stack.push('g'),
+Stack.push('4'),
+Stack.push('h'),
+Stack.push('gadc333'),
+console.log(Stack.elementlist);
+console.log(Stack.sizeof());
+Stack.pop();
+console.log(Stack.elementlist);
+console.log(Stack.sizeof());
